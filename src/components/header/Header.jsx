91ce0daab360bc7c10c44bdcbb012de79271d02a
Renata Kiraly <kirrena@gmail.com>
2024-01-07 21:09:48 +0000

    added starter code

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from './nav/Navbar';
+
+function Header() {
+    return (
+           <NavBar/>
+
+    )
+    }
+
+export default Header;    
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        It is me.
+      </p>
+    </div>
+  );
+}
+
+export default About;
